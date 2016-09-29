c98c7847debe1007a999eba3901bebe1ad4cafb4
Golovash <ttt@example.com>
2016-09-29 18:28:10 +0300

    added new 4 files

---
+++ main.js
@@ -0,0 +1,35 @@
+//create 23.09.2016
+/*
+    create Alex Golovash
+*/
+var hello="hello";
+var world=" world";
+var c=hello+world;
+console.log(c);
+var a1=5,a3=5;
+var a2=4;
+var sum=a1+a2;
+var min=a1-a2;
+var mult=a1*a2;
+var dilid=a1/a2;
+//a1=a1+1;
+a1++
+console.log(a1);
+//a3=a3-1;
+a3--;
+console.log(a3);
+var x=5,y=4,c;
+c=(x-y)*(x+y);
+console.log(c);
+
+var a=true;
+var b=false;
+var c,d;
+c=a||b;
+d=a&&b;
+//==
+//!=
+//if(a1==b1)
+//if(a1!=b1)
+//if(a1>=b1)
+//if(a1<=b1)
---
+++ main2.js
@@ -0,0 +1,45 @@
+'use strict'
+var a1=1;
+//console.log(a1);
+//var res=abcd(2);
+//function abcd(a){
+//    return a++;
+//}
+////var res=abcd(2);
+//console.log(res);
+
+//var a=function(b){
+//console.log(b);
+//}
+//a('hello');
+
+//function (){
+//    console.log('hello2');
+//}();
+
+//for(var i=5;i<7;i++){
+//    console.log(i);
+//}
+
+//var g='str'
+//typeof g;
+
+//var numb = function(num){
+//     if (typeof num === 'number'){
+//         if((num % 2)=== 0){     
+//             return '4et';
+//         } 
+//         else{
+//            return'ne4et';
+//         }
+//     } else {
+//         return 'error';
+//     }
+//
+//console.log(numb(2));
+//
+//for(var i=1;i<=1000;i++){
+//    console.log(i+" "+numb(i))
+//}
+function(a) 
+while()
