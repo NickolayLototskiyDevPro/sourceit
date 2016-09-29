9e3bbe608d699c2a4c762b12e3a7ebb93315d7a1
5s4zetH <anya.work97@gmail.com>
2016-09-30 01:08:45 +0300

    Add files via upload

--- HomeExercises/homework30_09.js
+++ HomeExercises/homework30_09.js
@@ -1,5 +1,5 @@
 //Buble sort
-	var BubleSort = [ 9, 4, 5, 2, 3, 1, 8, 7, 6, 5, 0, 0 ],
+	var BubleSort = [ 5, 1, 6, 7, 0, 65, -1 ],
 	i, j, k, n = BubleSort.length;
 	console.log('Begin with this array : ' + BubleSort);
 	console.log('Numbers of elements in array : ' + n);
@@ -14,23 +14,23 @@
 }
 console.log('New array: ' + BubleSort);
 //--------------------------------------------------------------------------
-//Buble sort throw the function
-	var BubleSort1 = function (arr) {
-	var a, j, i;
-	for ( j = 0; j < arr.length; j++) {
-	for ( i = 0; i < arr.length - 1; i++) {
-		if (arr[i] > arr[i + 1]){
-			a = arr[i];
-			arr[i] = arr[i + 1];
-			arr[i + 1] = a;
-				}	
-			}
-		}
-	}
-	var mass = [4,6,2,1,2,3,7,8,7,7];
-	console.log('Begin with this array : ' + mass);
-	console.log('Numbers of elements in array : ' + mass.length);
-	console.log('New array: ' + BubleSort1(mass));
+// //Buble sort throw the function
+// 	function BubleSort1 (mass) {
+// 	var a, j, i;
+// 	for ( j = 0; j < mass.length; j++) {
+// 	for ( i = 0; i < mass.length - 1; i++) {
+// 		if (mass[i] > mass[i + 1]){
+// 			a = mass[i];
+// 			mass[i] = mass[i + 1];
+// 			mass[i + 1] = a;
+// 				}	
+// 			}
+// 		}
+// 	}
+// 	var mass = [5, 1, 6, 7, 0, 65, -1];
+// 	console.log('Begin with this array : ' + mass);
+// 	mass.sort(BubleSort1);
+// 	console.log('New array: ' + mass);
 //--------------------------------------------------------------------------
 //How to mark bad words
 var str = 'The use of animals in research is not a new concept, for it began as early as in the second century by a Roman physician.It took nearly another eighteen hundred years for such experimentation to become a vital role in the advancement of both science and technology.Such advancements paved the way for the many experiments that are used in today’s generation as an investigative technique to explore the depths of the human body without actually exploiting the human.There are four principal reasons why animals continue to be a the first phase for any biomedical research project;those being to create a biological model from the animal only to further the knowledge of how a human body works, how a disease impacts the human body,how to create forms of treatment in relation to the human body, as well as testing the efficacy and safety of new drugs.';
@@ -47,3 +47,26 @@ for (var i = 0; i < array.length; i++) {
 var str = array.join(' ');    
 document.write('The paragraph that we have done : ' + str);
 //--------------------------------------------------------------------------
+//Sort with max and minelement
+	var SortMax = [ 5, 1, 6, 7, 0, 65, -1];
+	console.log('Begin with this array : ' + SortMax);
+	function sDecrease (i, ii) {
+		if (i < ii)
+			return 1;
+		else if (i > ii)
+			return -1;
+		else 
+			return 0;
+	}
+	SortMax.sort(sDecrease);
+	console.log('Our new array sorting by max: ' + SortMax);
+	function sIncrease(i, ii) {
+		if (i < ii)
+			return -1;
+		else if (i > ii) 
+			return 1;
+		else 
+				return 0;
+		}
+	SortMax.sort(sIncrease);
+	console.log('Our new array sorting by min : ' + SortMax);
