548ca85a3a04db91eaa59536bbdd267c7c65cae6
Light210191 <light211113@gmail.com>
2016-10-06 22:10:35 +0300

    Home Work arihmetical Progression

---
+++ arihmeticalProgression/arihmeticalProgression.js
@@ -0,0 +1,11 @@
+var firstElement = prompt("Enter first element");
+var lastElement = prompt("Enter last element");
+var firstNumber = Number(firstElement);
+var lastNumber = Number(lastElement);
+
+
+
+summArifmeticalProgression = 0;
+for (firstNumber ; firstNumber <= lastNumber; firstNumber++)
+{summArifmeticalProgression += firstNumber}
+console.log(summ);
