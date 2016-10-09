88386fb05e3918edf24465ae5bc02a7b6b66ed31
sedykhanton <sedykhanton@gmail.com>
2016-10-09 18:34:47 +0300

    Panzer 1.0

--- Panzer.js
+++ Panzer.js
@@ -3,8 +3,8 @@ function Tank (){
 
 	var MAX_SPEED = 60;
 	this.wariors = 4;
-	this.realSpeed = 60; 
-	this.ammunition = 3;
+	this.realSpeed = 200; 
+	this.ammunition = 1;
 	this.armor = 5;
 
 	this.fast = function(){
@@ -26,15 +26,23 @@ function Tank (){
   		console.log ("Ammunition left "+ amm)
 		}
 	
-	function speedcheck(){if  (this.ammunition  > 0, this.realSpeed <= 40 ) { alert("Bang!!")}
-		else {alert ("No ammo, or to high speed for fire")}}
+	function ammspeedcheck(){
+		if  (this.ammunition  <= 0) {
+			alert ( "No ammo");
+		}
+		if (this.realSpeed > 40 ) { 
+			alert ("Hi speed! decrease speed to 40 mp/h"); 
+		}
+		else {alert("Bang!")}
+		};
 	
 	function reload() { this.ammunition-1 }
 	
 	this.fire = function(){
-		setTimeout(speedcheck.call(this), display.call(this),reload.call(this))}
+		setTimeout(ammspeedcheck.call(this), display.call(this),reload.call(this))}
 }
 	
 	 
 var st = new Tank();
 st.fire();
+
