a69076358c90efad127114a35b43d09fbc9e0883
Testosterone250 <00diran000@mail.ru>
2016-10-10 17:07:46 +0300

    correct mistakes

--- dz7/index3.js
+++ dz7/index3.js
@@ -7,6 +7,7 @@ function Tank (){
  var Speed = 0;
  var MaxSpeed = 60;
  var ShootSpeed = 40;
+ var Bullets;
 
 this.boost = function (Speed){
 
@@ -23,12 +24,10 @@ this.boost = function (Speed){
 
 this.slow = function (Speed){
 	// I cant understand this construction?
- 	if (Speed < MaxSpeed){
  		Speed--;
  		console.log('Tank speed decreased!You current speed is ' + Speed + ' Km/H!');
  		alert('Tank speed decreased!You current speed is ' + Speed + ' Km/H!');
- 	}
-
+ 		
  	if (Speed <= 0){
  		alert('Bad news, Tank is stopped!');
  	}
@@ -36,20 +35,17 @@ this.slow = function (Speed){
 
 this.fire = function (Bullets){
 	// Use boolean operator && and add additional condition
-	if (Speed > ShootSpeed) {
-		alert('You cant fire on this speed!'); // не могу заставить работать ограничение по скорости на стрельбу!
-	}
-	// Do not use not operator it is hard to read
-	else if (Bullets <= 0){
-		alert('No bullets!');
-	}
+	if (Speed < ShootSpeed && Bullets > 0){
+		Bullets--;
 
- 	else {
- 		Bullets--;
- 		console.log('Count of left bullets is ' + Bullets);
+		console.log('Count of left bullets is ' + Bullets);
  		alert('Fire! You have ' + Bullets + ' bullets!');
- 	}
- }
+		}
+
+	else{
+ 		alert('You cant shoot on this speed!');
+ 		}
+	}	
 }
 // Use empty lines
 var cab = new Tank();
@@ -57,7 +53,7 @@ console.log('Armor is ' + cab.Armor + '%');
 console.log('Fuel is ' + cab.Fuel + '%');
 console.log('Crew is ' + cab.Crew + ' persons');
 // cab.boost(100);
-cab.boost(30);
+cab.boost(40);
 cab.slow(25);
 // cab.slow(0);
 cab.fire(5);
