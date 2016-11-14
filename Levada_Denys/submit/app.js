a95edea335b1d58591315db0e541d1b9b5d97a58
Litvinov Sergei <criogon@mail.ru>
2016-11-14 19:35:29 +0200

    submit pre-remaster

--- Levada_Denys/submit/app.js
+++ Levada_Denys/submit/app.js
@@ -3,37 +3,53 @@
 // Please rework this
 // General comment must be one return from each of your method! Multiple returns is not good style.
 
-function submit() {
-    checkName();
-    checkSecondName()
-    checkEmail();
-    checkNumber();
-}
-//A lot of code duplicates! Maybe worse create one function thats would recieve jquery element and regex? 
-function checkName() {
-    let str = $("#name").val();
-    let regExp = /^[A-z]{1,}$/;// Insert regex to global constant, this line repeat more twice
-    if (regExp.test(str)) return $("#name").css('border', '2px solid green');
-    else return $("#name").css('border', '2px solid red');
+const regExpStr = /^[A-z]{1,}$/;
+const regExpEmail = /^[A-z]{1,}@[A-z]{1,}.[A-z]{2,3}$/;
+const regExpNum = /^[0-9]{3}-[0-9]{2}-[0-9]{2}$/;
+
+class SubmitForm {
+    constructor() {
+
+    }
+
+    checkName() {
+        let str = $("#name").val();
+        regExpStr.test(str) ? $("#name").css('border', '2px solid green') : $("#name").css('border', '2px solid red');
+    }
+
+    checkSecondName() {
+        let str = $("#secondName").val();
+        regExpStr.test(str) ? $("#secondName").css('border', '2px solid green') : $("#secondName").css('border', '2px solid red');
+    }
+
+    checkEmail() {
+        let str = $("#email").val();
+        regExpEmail.test(str) ? $("#email").css('border', '2px solid green') : $("#email").css('border', '2px solid red');
+    }
+
+    checkNumber() {
+        let str = $("#telNumber").val();
+        regExpNum.test(str) ? $("#telNumber").css('border', '2px solid green') : $("#telNumber").css('border', '2px solid red');
+    }
+
+    checkAll() {
+        this.checkName();
+        this.checkSecondName();
+        this.checkEmail();
+        this.checkNumber();
+    }
+
+
+
+
+
 }
 
-function checkSecondName() {
-    let str = $("#secondName").val();
-    let regExp = /^[A-z]{1,}$/;// Insert regex to global constant, this line repeat more twice
-    if (regExp.test(str)) return $("#secondName").css('border', '2px solid green');// Use brackets in if. Or use ? statement.
-    else return $("#secondName").css('border', '2px solid red');
+
+var submitForm = new SubmitForm();
+
+function checkForm() {
+    submitForm.checkAll();
 }
 
-function checkEmail() {
-    let str = $("#email").val();
-    let regExp = /^[A-z]{1,}@[A-z]{1,}.[A-z]{2,3}$/;// Insert regex to global constant, this line repeat more twice
-    if (regExp.test(str)) return $("#email").css('border', '2px solid green');
-    else return $("#email").css('border', '2px solid red');
-}
-
-function checkNumber() {
-    let str = $("#telNumber").val();
-    let regExp = /^[0-9]{3}-[0-9]{2}-[0-9]{2}$/// Insert regex to global constant, this line repeat more twice
-    if (regExp.test(str)) return $("#telNumber").css('border', '2px solid green');
-    else return $("#telNumber").css('border', '2px solid red');
-}
+//A lot of code duplicates! Maybe worse create one function thats would recieve jquery element and regex?
