0e0cb5d2ce771ee3ce2772ecba6f831609f95cf9
Denys Levada <levada.denys@gmail.com>
2016-11-15 01:13:16 +0200

    submit remastered, js and jquery ways, complexity 5

--- Levada_Denys/submit/app.js
+++ Levada_Denys/submit/app.js
@@ -3,53 +3,41 @@
 // Please rework this
 // General comment must be one return from each of your method! Multiple returns is not good style.
 
+
 const regExpStr = /^[A-z]{1,}$/;
-const regExpEmail = /^[A-z]{1,}@[A-z]{1,}.[A-z]{2,3}$/;
+const regExpEmail = /^[A-z,0-9]{1,}@[A-z,0-9]{1,}.[A-z]{2,3}$/;
 const regExpNum = /^[0-9]{3}-[0-9]{2}-[0-9]{2}$/;
 
-class SubmitForm {
+
+class CheckForm {
     constructor() {
+        const regExpObj = {
+            name: regExpStr,
+            secondName: regExpStr,
+            email: regExpEmail,
+            telNumber: regExpNum,
+        }
 
+        this.check = function() {
+            let regExObjPropNames = Object.getOwnPropertyNames(regExpObj);
+            let regExObjValues = Object.values(regExpObj);
+
+            for (let i = 0; i < regExObjValues.length; i++) {
+                regExObjValues[i].test($("#" + regExObjPropNames[i]).val()) ?
+                    $("#" + regExObjPropNames[i]).removeClass('red').addClass('green') :
+                    $("#" + regExObjPropNames[i]).removeClass('green').addClass('red');
+            }
+
+
+                /////---------jquery way-------------\\\\
+
+            // $.each(regExpObj, function(locator, regex) {
+            //     regex.test($("#" + locator).val()) ? $("#" + locator).removeClass('red').addClass('green') :
+            //         $("#" + locator).removeClass('green').addClass('red');
+            // });
+        }
     }
-
-    checkName() {
-        let str = $("#name").val();
-        regExpStr.test(str) ? $("#name").css('border', '2px solid green') : $("#name").css('border', '2px solid red');
-    }
-
-    checkSecondName() {
-        let str = $("#secondName").val();
-        regExpStr.test(str) ? $("#secondName").css('border', '2px solid green') : $("#secondName").css('border', '2px solid red');
-    }
-
-    checkEmail() {
-        let str = $("#email").val();
-        regExpEmail.test(str) ? $("#email").css('border', '2px solid green') : $("#email").css('border', '2px solid red');
-    }
-
-    checkNumber() {
-        let str = $("#telNumber").val();
-        regExpNum.test(str) ? $("#telNumber").css('border', '2px solid green') : $("#telNumber").css('border', '2px solid red');
-    }
-
-    checkAll() {
-        this.checkName();
-        this.checkSecondName();
-        this.checkEmail();
-        this.checkNumber();
-    }
-
-
-
-
-
 }
 
-
-var submitForm = new SubmitForm();
-
-function checkForm() {
-    submitForm.checkAll();
-}
-
+var validateForm = new CheckForm();
 //A lot of code duplicates! Maybe worse create one function thats would recieve jquery element and regex?
