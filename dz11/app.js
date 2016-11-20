8f7d1e7ee52f4e9f7dcfcc4295957ea13dd2a3e6
Testosterone250 <000diran000@mail.ru>
2016-11-20 19:38:35 +0200

    new dz

--- dz11/app.js
+++ dz11/app.js
@@ -1,15 +1,18 @@
+'use strict'
 
-if (window.addEventListener){
+    if (window.addEventListener){
 
     window.addEventListener('load', init, false);
+
 }
 
-else if (window.attachEvent){
+    else if (window.attachEvent){
 
     window.attachEvent('onload', init);
+
 };
 
-function init(){
+	function init(){
 
     add.onclick = onAddHandler;
     clear.onclick = onClearHandler;
@@ -17,27 +20,46 @@ function init(){
 
 };
 
-    function onAddHandler() {
-
-    	let section = 1;
- 		$('<div class="addUser"><p>Result: <input type="text"></p></div>').attr("id", "section" + section++).appendTo("#childValidateForm");
-    };
+    let uniqueId = 1;
 
     function onClearHandler() {
 
       $('input').val('');
       $('.addUser').remove();
+      $('#name').css('color','black');
+
+      return uniqueId = 1;
 };
 
     function onValidateHandler() {
 	    
 	let name = $('#name').val();
-    	console.log(name);
     	
 	let regexp = $('#regexp').val();
-    	console.log(regexp);
 
-	let tmpEmail = name.search(regexp);
-		console.log(tmpEmail);
+    regexp = new RegExp(regexp);
 
+	let tmpName = name.search(regexp);
+
+        if (tmpName >= 0) {
+
+	       $('#name').css('color','green');
+
+        }
+
+         else {
+            
+           $('#name').css('color','red');
+        }
 };
+    
+    function onAddHandler() {
+
+        let name = $('#name').val();
+        let regexp = $('#regexp').val();
+
+        $('<div class="addUser">').appendTo("#childValidateForm");
+        $('<input type="text" class="addUser" size="2" disabled>').attr('value',"ID - " +uniqueId++).appendTo('#childValidateForm');
+        $('<input type="text" class="addUser" disabled>').attr('value', name).appendTo('#childValidateForm');
+        $('<input type="text" class="addUser" disabled>').attr('value', regexp).appendTo('#childValidateForm');
+    };
